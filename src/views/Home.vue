924702e07d4379466a5c83c2d3986081617ea1cd
嘉然今天吃什么 <jsclndnz@outlook.com>
2022-04-05 19:59:33 +0800

    定位

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <main ref="main">
-    <canvas id="canvas" :width="w" :height="h">
+    <canvas
+      id="canvas" :width="w" :height="h"
+      @wheel="onWheel"
+      @mousemove="onMouseMove"
+    >
     </canvas>
-    <div id="zoom" :style="{transform: `scale(${ratio})`}">
+    <div class="bottom">
+      <n-card size="small"> {{ x > 0 && y > 0 ? `${x}, ${y}` : '-, -' }}</n-card>
     </div>
+    <!--    <div id="zoom" :style="{transform: `scale(${ratio})`}">-->
+    <!--    </div>-->
   </main>
 </template>
 
@@ -24,14 +31,15 @@
     },
     data() {
       return {
-        data: [],
         // imgSrc: '/api/picture',
         imgSrc: 'img/ran.png',
-        size: 0,
+        originalSize: 0,
         w: 0,  // canvas width
         h: 0,  // canvas height
         dx: 0,  // The x coordinate in the canvas at which to place the top-left corner of the source image
         dy: 0,  // The y coordinate in the canvas at which to place the top-left corner of the source image
+        x: 0,  // x coordinate of the pixel
+        y: 0,  // y coordinate of the pixel
         ratio: 1,
         canvas: null,
         ctx: null,
@@ -39,7 +47,7 @@
     },
     computed: {
       currentSize() {
-        return Math.floor(this.size * this.ratio)
+        return Math.floor(this.originalSize * this.ratio)
       }
     },
     methods: {
@@ -47,34 +55,36 @@
         this.ctx.fillStyle = 'white'
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
       },
-      drawImage(
-        r = 1,
-        x = Math.floor(this.size / 2),
-        y = Math.floor(this.size / 2),
-      ) {
-        this.dx = this.dx - (r - 1) * x
-        this.dy = this.dy - (r - 1) * y
+      drawImage(r = 1, x = this.x, y = this.y) {
+        this.dx = this.dx - (r - this.ratio) * x
+        this.dy = this.dy - (r - this.ratio) * y
+        this.ratio = r
         this.ctx.imageSmoothingEnabled = false
         this.clearImage()
         this.ctx.drawImage(this.bitmap, this.dx, this.dy, this.currentSize, this.currentSize)
+      },
+      onMouseMove(e) {
+        const calculate = (offset, d) => {
+          return Math.ceil((offset - d) / this.ratio)
+        }
+        this.x = calculate(e.offsetX, this.dx)
+        this.y = calculate(e.offsetY, this.dy)
       },
       async onMounted() {
         let img = new Image()
         img.src = this.imgSrc
         img.crossOrigin = 'Anonymous'
         await new Promise((r) => (img.onload = r))
-        this.size = img.width
-        this.dx = Math.floor((this.w - this.size) / 2)
-        this.dy = Math.floor((this.h - this.size) / 2)
+        this.originalSize = img.width
+        this.dx = Math.floor((this.w - this.originalSize) / 2)
+        this.dy = Math.floor((this.h - this.originalSize) / 2)
         this.bitmap = await createImageBitmap(img)
-        this.drawImage()
+        this.drawImage(1, Math.floor(this.originalSize / 2), Math.floor(this.originalSize / 2))
       },
       async onWheel(e) {
         // this.canvas.style.transformOrigin = `${e.offsetX}px ${e.offsetY}px`
-        let level = (-e.deltaY / 200) * 0.25 // 正值为放大，负值为缩小
-        this.ratio = Math.max(this.ratio * (1 + level), 1)
-
-        this.drawImage(this.ratio)
+        let level = -e.deltaY > 0 ? 1 : -1 // 正值为放大，负值为缩小
+        this.drawImage(Math.max(this.ratio + level, 1))
         // this.ctx.scale(ratio, ratio)
       },
     },
@@ -84,7 +94,6 @@
       this.h = this.$refs.main.offsetHeight
       this.ctx = this.canvas.getContext('2d', {alpha: false})
       this.onMounted()
-      this.canvas.addEventListener('wheel', this.onWheel, false)
     },
   }
 </script>
