37b2a2c114f8200db2b9b376b6f7eaf143f66025
嘉然今天吃什么 <jsclndnz@outlook.com>
2022-04-05 20:45:53 +0800

    平移

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@
       id="canvas" :width="w" :height="h"
       @wheel="onWheel"
       @mousemove="onMouseMove"
+      @mousedown="isMoving = true"
+      @mouseup="isMoving = false"
     >
     </canvas>
     <div class="bottom">
@@ -40,6 +42,7 @@
         dy: 0,  // The y coordinate in the canvas at which to place the top-left corner of the source image
         x: 0,  // x coordinate of the pixel
         y: 0,  // y coordinate of the pixel
+        isMoving: false,
         ratio: 1,
         canvas: null,
         ctx: null,
@@ -55,7 +58,7 @@
         this.ctx.fillStyle = 'white'
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
       },
-      drawImage(r = 1, x = this.x, y = this.y) {
+      drawImage(r = this.ratio, x = this.x, y = this.y) {
         this.dx = this.dx - (r - this.ratio) * x
         this.dy = this.dy - (r - this.ratio) * y
         this.ratio = r
@@ -63,13 +66,41 @@
         this.clearImage()
         this.ctx.drawImage(this.bitmap, this.dx, this.dy, this.currentSize, this.currentSize)
       },
+      moveImage(x, y) {
+        this.dx += x
+        this.dy += y
+        this.drawImage()
+      },
+      onKeyDown(e) {
+        console.log(e.key)
+        const px = 25
+        if (e.key === 'w' || e.key === 'ArrowUp') {
+          this.moveImage(0, px)
+        } else if (e.key === 'a' || e.key === 'ArrowLeft') {
+          this.moveImage(px, 0)
+        } else if (e.key === 's' || e.key === 'ArrowDown') {
+          this.moveImage(0, -px)
+        } else if (e.key === 'd' || e.key === 'ArrowRight') {
+          this.moveImage(-px, 0)
+        }
+      },
       onMouseMove(e) {
+        if (this.isMoving) {
+          this.moveImage(e.movementX, e.movementY)
+        }
         const calculate = (offset, d) => {
           return Math.ceil((offset - d) / this.ratio)
         }
         this.x = calculate(e.offsetX, this.dx)
         this.y = calculate(e.offsetY, this.dy)
       },
+      async onWheel(e) {
+        // this.canvas.style.transformOrigin = `${e.offsetX}px ${e.offsetY}px`
+        let level = -e.deltaY > 0 ? 1 : -1 // 正值为放大，负值为缩小
+        this.drawImage(Math.max(this.ratio + level, 1))
+        // this.ctx.scale(ratio, ratio)
+      }
+      ,
       async onMounted() {
         let img = new Image()
         img.src = this.imgSrc
@@ -80,13 +111,8 @@
         this.dy = Math.floor((this.h - this.originalSize) / 2)
         this.bitmap = await createImageBitmap(img)
         this.drawImage(1, Math.floor(this.originalSize / 2), Math.floor(this.originalSize / 2))
-      },
-      async onWheel(e) {
-        // this.canvas.style.transformOrigin = `${e.offsetX}px ${e.offsetY}px`
-        let level = -e.deltaY > 0 ? 1 : -1 // 正值为放大，负值为缩小
-        this.drawImage(Math.max(this.ratio + level, 1))
-        // this.ctx.scale(ratio, ratio)
-      },
+      }
+      ,
     },
     mounted() {
       this.canvas = document.getElementById('canvas')
@@ -94,7 +120,9 @@
       this.h = this.$refs.main.offsetHeight
       this.ctx = this.canvas.getContext('2d', {alpha: false})
       this.onMounted()
-    },
+      document.addEventListener('keydown', this.onKeyDown)
+    }
+    ,
   }
 </script>
 
